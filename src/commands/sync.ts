8716f28a94b6d7c28663b1009d4a38522d7258bd
thew1thered21 <orewaee@gmail.com>
2022-06-03 18:14:00 +0400

    Added permission check

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -1,12 +1,20 @@
-import { donorGuildId } from "../../config.json";
+import { donorGuildId, confidants } from "../../config.json";
 import updateDatabase from "../utils/updateDatabase";
 import updateAllGuilds from "../utils/updateAllGuilds";
 import updateOneGuildOld from "../utils/updateOneGuild";
+import getMemberId from "../utils/getMemberId";
 
 export default async function sync( interaction ) {
-    const guild = interaction.guild;
+    const
+        guild = interaction.guild,
+        member = interaction.member;
 
     if ( guild.id == donorGuildId ) {
+        if ( !confidants.includes( getMemberId( member ) ) ) return interaction.reply( {
+            content: "You don't have enough rights",
+            ephemeral: true
+        } );
+
         await updateDatabase();
         await updateAllGuilds();
     } else {
